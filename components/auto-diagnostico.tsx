a63a3ed9e48f9d7938c3757af542bcca07fea340
jorgehara <empresamodelo2018@gmail.com>
2023-05-14 13:02:13 -0300

    commit botones a encuesta1

--- components/auto-diagnostico.tsx
+++ components/auto-diagnostico.tsx
@@ -19,7 +19,7 @@ const AutoDiagnostico = () => {
           />
         <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
           <a
-            href="https://quizify.us/quizzes/3201?gpt=true&topic=Encuesta%20Laboral&language=es-ES"
+            href="https://quizify.us/quizzes/3220?gpt=true&topic=preguntas%20a%20emprendedores%20para%20identificar%20en%20que%20rubro%20se%20los%20puede%20nuclear%20para%20encuandrarlos%20en%204%20categorias%20principales%20a-industrial%20b-tecnologico%20c-comunicacion%20d-turismo%20y%20viaje&language=es-ES"
             target="_blank"
             className="mx-3 bg-green-400 hover:bg-white hover:text-green-400 border border-green-400 text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
           >
